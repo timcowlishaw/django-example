986126eb70b16596b6cb373d5949620014d3349b
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2022-10-24 09:28:25 +0100

    Add our first page generated from a template!
    This is where the real web development work starts. We add a simple 'hello world' page
    which is served at the root url "/" of our site. To do this, we add three things:
    
     * The HTML template in `pages/templates/home.html` - this is just a regular html page
     * A new *view* in `pages/views.py`, this is a simple python function that renders that template
     * An extra line in `pages/urls.py` which tells django that the view we just wrote should be used to answer all requests for the homepage / root url ("/").

--- pages/urls.py
+++ pages/urls.py
@@ -6,8 +6,16 @@
 # allows us to define a path in the url (ie the bit after the first slash).
 from django.urls import path
 
+# We also need to import the views we've written in views.py so that we can wire them up to urls:
+# the "." (dot) here signifies that we're importing from a views module that's inside the same folder
+# as we're in currently.
+from .views import home
+
 # We then make a list of 'url patterns' which define our urls. Django comes looking
-# for this object when we include the file. We'll leave it empty for now as we
-# don't yet have any views (the square brackets are used to make a list of things,
-# in this case it has nothhng inside).
-urlpatterns = []
+# for this object when we include the file.
+urlpatterns = [
+    # We add a pattern which matches our homepage path.
+    # The homepage has a blank path ("ie there's nothing after the "/" (slash), so
+    # this pattern, with a blank path, matches the homepage, and renders our "home" view.
+    path("", home, name="home"),
+]
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# Let's create a view for our homepage. This is a python _function_, a little block
+# of code / behaviour that can be reused and called from elsewhere - so it starts with the `def`
+# keyword, short for `define` Our function is called `home` (but we could have called it whatever we like), and it takes a single _argument_, like
+# an option or parameter, that django will pass to it when a user requests the page.
+# This argument represents details about the http request the user makes, and while we don't need to concern ourselves too much with it,
+# we need it as we'll need to pass it back to django later.
+def home(request):
+    # Inside our function, we just want to tell django to repsond with the html template we've created. We can use the
+    # `render` function that django helpfully imports for us above in order to do that - it takes the request we received earlier as
+    # an argument, and the name of the template we want to render. - (django automatically looks in the 'templates' directory of the
+    # current application for these). This creates a `response` object which we `return` to django,
+    # showing that we've finished out work here, and Django can continue processing the request.
+    return render(request, "home.html")
